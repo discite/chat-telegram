36add8b2225ef783cf2d2d769d69983bad5ad9f7
Jeancarlo <discite@protonmail.com>
2018-03-18 04:55:48 +0000

    Update webhook.php

--- public/webhook.php
+++ public/webhook.php
@@ -39,3 +39,11 @@ function sendMessage($chatId, $text)
   $response = file_get_contents("$TELEGRAM/sendMessage?$query");
   return $response;
 }
+
+$request = file_get_contents("php://input");
+
+$date = date('Y-m-d H:i:s');
+
+$content = "$date\n$request\n\n";
+
+file_put_contents("webhook.log", $content, FILE_APPEND);
